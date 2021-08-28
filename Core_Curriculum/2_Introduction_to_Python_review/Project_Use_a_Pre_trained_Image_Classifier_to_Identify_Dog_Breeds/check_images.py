5edfed8fb6dc978e864f4032b90cbef32b04e124
Kais Chebata <qays.sh@gmail.com>
2021-08-28 22:02:12 +0100

    Adding get_input_args() function
    get_input_args Retrieves and parses command line arguments from the user

--- Core_Curriculum/2_Introduction_to_Python_review/Project_Use_a_Pre_trained_Image_Classifier_to_Identify_Dog_Breeds/check_images.py
+++ Core_Curriculum/2_Introduction_to_Python_review/Project_Use_a_Pre_trained_Image_Classifier_to_Identify_Dog_Breeds/check_images.py
@@ -115,7 +115,7 @@ def main():
     # and incorrectly classified breeds (if requested)
     print_results(results, results_stats, None, True, True)
     
-    sleep(10)
+    
     # TODO 0: Measure total program runtime by collecting end time
     end_time = time()
     
